f7a257d6a78d4faa845f5c8f166d80fb09d6fef6
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2024-11-02 10:04:26 -0300

    feat: first commit

---
+++ frontend/festive/src/app/domain/model/event/Event.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: number;
+    date: Date;
+    description: string;
+    title: string;
+    photo: string;
+    city: String;
+    neighborhood: string;
+    number: string;
+    street: string;
+}
---
+++ frontend/festive/src/app/pages/main/main.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import {FormsModule} from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
+
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import * as fontawesome from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    FontAwesomeModule,
+    NavBarComponent
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  faCoffee = fontawesome.faHeartBroken;
+
+  constructor(private router: Router) {
+  }
+
+  logout() {
+  // this.authenticationService.logout();
+  // this.router.navigate(['account/sign-in']);
+  }
+
+}
